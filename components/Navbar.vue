b8dae8bc571ae7d59adae08d0165f438cb9fa59d
Robrens <jbb.baudu@wanadoo.fr>
2019-05-06 15:02:54 +0200

    edit nav and link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,18 +5,9 @@
       <li><a href="#techno">Connaissance</a></li>
       <li><a href="#project">Projets</a></li>
     </ul>
-    <ul v-if="this.$route.name =='lang-portfolio'">
-      <li><nuxt-link to="cv">Curiculum Vitae</nuxt-link></li>
-      <li><nuxt-link to="contact">Contact</nuxt-link></li>
-    </ul>
-    <ul v-else-if="this.$route.name =='lang-cv'">
-      <li><nuxt-link to="portfolio">Portfolio</nuxt-link></li>
-      <li><nuxt-link to="contact">Contact</nuxt-link></li>
-    </ul>
-    <ul v-if="this.$route.name =='lang-contact'">
-      <li><nuxt-link to="portfolio">Portfolio</nuxt-link></li>
-      <li><nuxt-link to="cv">Curiculum Vitae</nuxt-link></li>
-    </ul>
+    <div>
+      <a href="">download CV</a>
+    </div>
     <div class="swith-language" v-if="$route.params.lang == 'fr'">
       <div class="select-language"
       ref="button"
@@ -31,13 +22,7 @@
         exclude: ['button'],
         handler: 'onClose'
       }">
-      <router-link v-if="$route.fullPath === '/fr/portfolio'" to="/en/portfolio">
-        <img src="/english_flag.png" alt="english">
-      </router-link>
-      <router-link v-else-if="$route.fullPath === '/fr/cv'" to="/en/cv">
-        <img src="/english_flag.png" alt="english">
-      </router-link>
-      <router-link v-else to="/en/contact">
+      <router-link to="/en/portfolio">
         <img src="/english_flag.png" alt="english">
       </router-link>
       </div>
@@ -56,13 +41,7 @@
         exclude: ['button'],
         handler: 'onClose'
       }">
-      <router-link v-if="$route.fullPath === '/en/portfolio'" to="/fr/portfolio">
-        <img src="/french_flag.png" alt="french">
-      </router-link>
-      <router-link v-else-if="$route.fullPath === '/en/cv'" to="/fr/cv">
-        <img src="/french_flag.png" alt="french">
-      </router-link>
-      <router-link v-else to="/fr/contact">
+      <router-link to="/fr/portfolio">
         <img src="/french_flag.png" alt="french">
       </router-link>
       </div>
--- pages/_lang/portfolio.vue
+++ pages/_lang/portfolio.vue
@@ -3,7 +3,27 @@
     <navbarView></navbarView>
     <v-layout>
       <v-flex text-xs-center>
-
+        <div id="about">
+          <h2 class="title">Présentation</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta inventore reiciendis, eum animi ea tenetur accusantium adipisci? Rem, ducimus at tempora cum autem sit quos officiis! Nulla, mollitia illum? Quam?</p>
+        </div>
+        <div id="techno">
+          <h2 class="title">technologies</h2>
+          <ul>
+            <li>plop</li>
+            <li>plop</li>
+            <li>plop</li>
+            <li>plop</li>
+            <li>plop</li>
+            <li>plop</li>
+            <li>plop</li>
+            <li>plop</li>
+          </ul>
+        </div>
+        <div id="project">
+          <h2 class="title">réalisations</h2>
+          <p>In coming</p>
+        </div>
       </v-flex>
     </v-layout>
   </section>
